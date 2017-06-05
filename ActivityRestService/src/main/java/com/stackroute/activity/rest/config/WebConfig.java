537b4e1a973e448ae2486453f391e9b8d723779c
somsubhra.m <somsubhra.m@NIITEDUCATION.COM>
2017-06-05 12:16:43 +0530

    image and file upload functionality implemented. However, the file is not yet displayed in the posts and in the modal.

--- ActivityRestService/src/main/java/com/stackroute/activity/rest/config/WebConfig.java
+++ ActivityRestService/src/main/java/com/stackroute/activity/rest/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.stackroute.activity.rest.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -12,5 +13,13 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan(basePackages="com.stackroute")
 public class WebConfig {
 
+	@Bean(name="multipartResolver")
+	public CommonsMultipartResolver getMultipartResolver()
+	{
+		CommonsMultipartResolver multipartResolver=new CommonsMultipartResolver();
+		multipartResolver.setMaxUploadSize(268435456);
+		
+		return multipartResolver;
+	}
 	
 }
---
+++ ActivityStreamBackEnd/src/main/java/com/stackroute/activity/model/FileInfo.java
@@ -0,0 +1,33 @@
+package com.stackroute.activity.model;
+
+public class FileInfo extends BaseDomain {
+
+	private String fileName;
+	private long fileSize;
+	private String fileType;
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public long getFileSize() {
+		return fileSize;
+	}
+
+	public void setFileSize(long fileSize) {
+		this.fileSize = fileSize;
+	}
+
+	public String getFileType() {
+		return fileType;
+	}
+
+	public void setFileType(String fileType) {
+		this.fileType = fileType;
+	}
+
+}
