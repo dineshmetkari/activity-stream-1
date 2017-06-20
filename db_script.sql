598b4dd7d446574eb0dd361166ee3de0ea9c08e2
somsubhra.m <somsubhra.m@NIITEDUCATION.COM>
2017-06-20 15:08:15 +0530

    modified circle.yml file

--- db_script.sql
+++ db_script.sql
@@ -1,25 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `activity_stream_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `activity_stream_db`;
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: activity_stream_db
--- ------------------------------------------------------
--- Server version	5.7.18-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `circle`
---
 
 DROP TABLE IF EXISTS `circle`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
